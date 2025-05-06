ffb8ba00b30b85ce6d826df6de04cfd69c66ec00
Santiagomunozma <santiagomunozma@gmail.com>
2025-05-05 20:01:14 -0500

    feat: add coupon model, controller, service, validation, and routes

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -1,61 +1,68 @@
-import { Request, Response } from 'express';
-import { createProductSchema, updateProductSchema } from './product.validation';
-import { Product } from './product.model';
-import { productsService } from './product.service';
+import { Request, Response } from "express";
+import { createProductSchema, updateProductSchema } from "./product.validation";
+import { Product } from "./product.model";
+import { productsService } from "./product.service";
 
 const createProduct = async (req: Request, res: Response) => {
-    const result = createProductSchema.safeParse(req.body);
+  console.log("Creating product...", req.body);
+  const result = createProductSchema.safeParse(req.body);
 
-    if (!result.success) {
-        res.status(400).json({ error: result.error.errors });
-        return;
-    }
+  if (!result.success) {
+    res.status(400).json({ error: result.error.errors });
+    return;
+  }
 
-    const product = await productsService.createProduct(result.data);
-    res.status(201).json(product);
-}
+  const product = await productsService.createProduct(result.data);
+  res.status(201).json(product);
+};
 
 const updateProduct = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    
-    const result = updateProductSchema.safeParse({ ...req.body, id });
+  const { id } = req.params;
 
-    if (!result.success) {
-        res.status(400).json({ error: result.error.errors });
-        return;
-    }
+  const result = updateProductSchema.safeParse({ ...req.body, id });
 
-    const product = await productsService.updateProduct(result.data as Product);
-    res.status(200).json(product);
+  if (!result.success) {
+    res.status(400).json({ error: result.error.errors });
+    return;
+  }
+
+  const product = await productsService.updateProduct(result.data as Product);
+  res.status(200).json(product);
 };
 
 const deleteProduct = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!id) {
-        res.status(400).json({ error: 'Product ID is required' });
-        return;
-    }
+  if (!id) {
+    res.status(400).json({ error: "Product ID is required" });
+    return;
+  }
 
-    const product = await productsService.deleteProduct({ id } as Product);
-    res.status(200).json(product);
+  const product = await productsService.deleteProduct({ id } as Product);
+  res.status(200).json(product);
 };
 
 const getProductById = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!id) {
-        res.status(400).json({ error: 'Product ID is required' });
-        return;
-    }
+  if (!id) {
+    res.status(400).json({ error: "Product ID is required" });
+    return;
+  }
 
-    const product = await productsService.findProductById(id);
-    res.status(200).json(product);
+  const product = await productsService.findProductById(id);
+  res.status(200).json(product);
 };
 
 const getAllProducts = async (req: Request, res: Response) => {
-    const products = await productsService.findAll();
-    res.status(200).json(products);
+  const products = await productsService.findAll();
+  res.status(200).json(products);
 };
 
-export { createProduct, updateProduct, deleteProduct, getProductById, getAllProducts };
+export {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductById,
+  getAllProducts,
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { userRoutes } from "../modules/users/user.routes";
-import { productRoutes} from "../modules/products/product.route";
+import { productRoutes } from "../modules/products/product.route";
 import { sellRoutes } from "../modules/sell/sell.routes";
 
 const router = Router();
@@ -8,8 +8,6 @@ const router = Router();
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
 router.use("/sells", sellRoutes);
+router.use("/coupons", sellRoutes);
 
 export default router;
-
-
-
